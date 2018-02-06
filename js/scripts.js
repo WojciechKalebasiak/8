8e30e87a8a300b1025b7aa60e83801c8a44eaa43
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-02-06 08:44:45 +0100

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 var name = prompt('Enter your name');
-alert("Hello, " + name);
+//alert("Hello, " + name);
 console.log("Hello, " + name);
+//Line comment
+/* WOW DODGE DODGE
+
+
+SO MANY WOW
+
+
+LINES */
